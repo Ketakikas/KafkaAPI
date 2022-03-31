06a96e1a724378fcfdf5755abe802631cbfb1a94
Ketaki Kashyap <Ketakikashyap1991@gmail.com>
2022-03-31 11:37:50 +0530

    initial commit

---
+++ KafkaWebApi/Controllers/ProducerController.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+using KafkaWebApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+
+namespace KafkaWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProducerController : ControllerBase
+    {
+        private ProducerConfig _config;
+        public ProducerController(ProducerConfig config)
+        {
+            _config = config;
+        }
+        [HttpPost("send")]
+        public async Task<ActionResult> Get(string topic,[FromBody]Employee employee)
+        {
+            string serializedEmployee = JsonConvert.SerializeObject(employee);
+            using (var producer = new ProducerBuilder<Null, string>(_config).Build())
+            {
+                await producer.ProduceAsync(topic, new Message<Null, string> { Value = serializedEmployee });
+                producer.Flush(TimeSpan.FromSeconds(10));
+                return Ok(true);
+            }
+        }
+    }
+}
